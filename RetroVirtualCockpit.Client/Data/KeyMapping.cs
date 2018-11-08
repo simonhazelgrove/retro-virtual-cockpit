bcc94a13a14d8906a4efcd4c5b4dab7442f2c470
Simon Hazelgrove <simonhazelgrove@hotmail.com>
2018-11-08 21:02:34 +1000

    Initial version

---
+++ RetroVirtualCockpit.Client/Data/KeyMapping.cs
@@ -0,0 +1,32 @@
+﻿using WindowsInput.Native;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace RetroVirtualCockpit.Client.Data
+{
+    public class KeyMapping
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public VirtualKeyCode KeyCode { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public VirtualKeyCode? ModifierKeyCode { get; set; }
+
+        public KeyMapping()
+        {
+        }
+
+        public KeyMapping(VirtualKeyCode keyCode)
+        {
+            KeyCode = keyCode;
+            ModifierKeyCode = null;
+        }
+
+        public KeyMapping(VirtualKeyCode modifierKeyCode, VirtualKeyCode keyCode)
+        {
+            KeyCode = keyCode;
+            ModifierKeyCode = modifierKeyCode;
+        }
+    }
+}
